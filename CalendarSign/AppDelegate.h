1d82967352051575f7e6d5b6fb68b8489dcd5265
CurryYi <int_yi@163.com>
2017-05-31 13:19:02 +0800

    Initial Commit

---
+++ CalendarSign/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CalendarSign
+//
+//  Created by WYN on 2017/5/31.
+//  Copyright © 2017年 WYN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CalendarSign/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CalendarSign
+//
+//  Created by WYN on 2017/5/31.
+//  Copyright © 2017年 WYN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
